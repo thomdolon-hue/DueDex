d4664bbb77d6b0d93673551d28b056d50b3fd272
thomdolon-hue <thom.dolon@gmail.com>
2026-05-20 23:59:56 +0800

    force update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from "react"; // v2
 
 const BUSINESS_MODELS = [
   "Adult / Entertainment","CBD / Hemp","Cryptocurrency / Crypto Exchange","Forex / FX Trading",
